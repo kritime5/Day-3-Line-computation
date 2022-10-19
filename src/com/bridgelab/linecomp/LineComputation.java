7f6a7429848c293eb5b83638ba0e8809b1d0fb2b
Kritika <kritsme963@gmail.com>
2022-10-19 00:49:18 -0700

    Added:length of two cartesian  points

--- src/com/bridgelab/linecomp/LineComputation.java
+++ src/com/bridgelab/linecomp/LineComputation.java
@@ -1,7 +1,23 @@
 package com.bridgelab.linecomp;
 
+import static java.lang.Math.sqrt;
+import java.util.*;
+
 public class LineComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to line computation");
+        //uc1-model  a line based on point x,y co-ordinate
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value:");
+        int x1= sc.nextInt();
+        int x2= sc.nextInt();
+        int y1= sc.nextInt();
+        int y2= sc.nextInt();
+
+
+        double length = sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        System.out.println("Length of line:"+length);
+        
+
     }
 }
